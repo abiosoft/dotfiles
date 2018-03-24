a48d2bb12b7fdab92d0ee61189ca93f3cf58fc44
Abiola Ibrahim <abiola89@gmail.com>
2018-03-24 17:13:46 +0000

    modify xrandr

--- xrandr/home.sh
+++ xrandr/home.sh
@@ -1,4 +1,2 @@
 #!/bin/sh
-xrandr --output HDMI-2 --off --output HDMI-1 --off --output DP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output eDP-1 --mode 1920x1080 --pos 1920x0 --rotate normal --output DP-2 --off
-xrandr --output eDP-1 --scale .7x.7
-
+xrandr --output VIRTUAL1 --off --output eDP1 --mode 1368x768 --pos 1920x312 --rotate normal --output DP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI2 --off --output HDMI1 --off --output DP2 --off
