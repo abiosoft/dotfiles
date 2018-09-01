c1c36f086b03ca0a6ab3ce6d346b567dbb35ad79
Abiola Ibrahim <abiola89@gmail.com>
2018-09-01 07:01:10 +0100

    add installer to zsh

---
+++ zsh/install.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+echo installing zsh...
+sudo pacman -Sy --noconfirm zsh
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+
+echo installing oh-my-zsh...
+sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+
+cp abiola.zsh-theme $HOME/.oh-my-zsh/themes
+cp zshrc $HOME/.zshrc
