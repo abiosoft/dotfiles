f8fd1b749a55e5972de33e50e6677fc4c21448b0
Abiola Ibrahim <abiola89@gmail.com>
2018-10-04 21:01:46 +0100

    remove workspace label update

--- i3/move2workspace.sh
+++ i3/move2workspace.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ID=$(($(i3-msg -t get_workspaces | tr , '\n' | grep '"num":' | cut -d : -f 2 | sort -rn | head -1) + 1))
-i3-msg move container to workspace "[  $ID  ]"
-i3-msg workspace "[  $ID  ]"
+i3-msg move container to workspace $ID
+i3-msg workspace $ID
 
 
