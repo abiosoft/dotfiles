04e5b7ffef0fc4f12f70b2acaaca2e1dd328f5f2
Abiola Ibrahim <abiola89@gmail.com>
2018-10-05 01:04:35 +0100

    add rofi to i3

---
+++ i3/deps-ubuntu.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+sudo apt -y install feh rofi i3 dconf-cli
+
+
+
--- i3/install.sh
+++ i3/install.sh
@@ -14,3 +14,19 @@ cp Xresources $HOME/.Xresources
 echo setting up i3status...
 mkdir -p $HOME/.config/i3status
 cp i3status/config $HOME/.config/i3status/config
+
+echo setting up rofi...
+mkdir -p $HOME/.config/rofi
+cp rofi/config $HOME/.config/rofi/config
+mkdir -p $HOME/.local/share/rofi/themes
+cp rofi/tomorrow-night.rasi $HOME/.local/share/rofi/themes
+
+echo setting up i3 in mate...
+# remove close button
+dconf write /org/mate/desktop/interface/gtk-decoration-layout "'menu'"
+# set i3 as window manager
+dconf write /org/mate/desktop/session/required-components/windowmanager "'i3'"
+# disable desktop
+dconf write /org/mate/desktop/background/show-desktop-icons "false"
+# remove panels from components
+dconf write /org/mate/desktop/session/required-components-list "['windowmanager', 'filemanager', 'dock']"
