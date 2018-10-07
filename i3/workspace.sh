bdde816f09625460071a20c0b891743178a16633
Abiola Ibrahim <abiola89@gmail.com>
2018-10-07 17:15:50 +0100

    cycle workspace on scroll

--- i3/workspace.sh
+++ i3/workspace.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-i3-msg workspace $(($(i3-msg -t get_workspaces | tr , '\n' | grep '"num":' | cut -d : -f 2 | sort -rn | head -1) + 1))
+ID=$(($(i3-msg -t get_workspaces | tr , '\n' | grep '"num":' | cut -d : -f 2 | sort -rn | head -1) + 1))
+i3-msg workspace $ID
+[ -z "$1" ] || i3-msg move workspace to output $1
 
 
