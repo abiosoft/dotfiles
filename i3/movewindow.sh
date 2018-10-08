56d997f0d168a43f2582522b525a16ed72e62e56
Abiola Ibrahim <abiola89@gmail.com>
2018-10-08 10:09:24 +0100

    modify window movements

--- i3/movewindow.sh
+++ i3/movewindow.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-[ "$1" = "window" ] && i3-msg mode "Move Window. arrow key: within visible workspaces. shift+arrow key: across all workspaces"
-[ "$1" = "display" ] && i3-msg mode "Move to Monitor. arrow key: move window. shift+arrow key: move workspace"
+[ "$1" = "window" ] && i3-msg mode "Move Window. left right up down: across visible workspaces. shift+left right: across all workspaces. shift+up: new workspace"
+[ "$1" = "display" ] && i3-msg mode "Move to Monitor. left right up down: move window. shift+left right up down: move workspace"
