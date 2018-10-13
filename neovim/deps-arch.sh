87637ae25785a386e8701b7a72aa333caff0e1d9
Abiola Ibrahim <abiola89@gmail.com>
2018-10-13 11:07:12 +0100

    add rope for python refactoring

--- neovim/deps-arch.sh
+++ neovim/deps-arch.sh
@@ -2,7 +2,7 @@
 
 echo installing neovim and deps...
 sudo pacman -Sy --noconfirm curl neovim python-pip python2-pip ctags && \
-    pip install --user neovim autopep8 pylint --upgrade && \
-    pip2 install --user neovim autopep8 pylint --upgrade
+    pip install --user neovim autopep8 pylint rope --upgrade && \
+    pip2 install --user neovim autopep8 pylint rope --upgrade
 
 
--- neovim/deps-ubuntu.sh
+++ neovim/deps-ubuntu.sh
@@ -2,7 +2,7 @@
 
 echo installing neovim and deps...
 sudo apt -y install neovim curl python-pip python3-pip ctags && \
-    pip2 install --user neovim autopep8 pylint --upgrade && \
-    pip3 install --user neovim autopep8 pylint --upgrade
+    pip2 install --user neovim autopep8 pylint rope --upgrade && \
+    pip3 install --user neovim autopep8 pylint repe --upgrade
 
 
