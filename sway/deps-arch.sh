11b6e0544ad7ad56187b794d0cabc7a2c717d537
Abiola Ibrahim <abiola89@gmail.com>
2018-10-21 21:19:19 +0100

    add keyboard layout

--- sway/deps-arch.sh
+++ sway/deps-arch.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
-sudo pacman -Sy --noconfirm mate-media yay light feh
+sudo pacman -Sy --noconfirm mate-media yay light feh networkmanager network-manager-applet
 
-yay -Sy --noconfirm slurp-git grim-git mako-git 
+yay -Sy --noconfirm slurp-git grim-git mako-git
+
