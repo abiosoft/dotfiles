f5c3adbfb5ebe9cd45f398042e43edd0c76b7947
Abiola Ibrahim <abiola89@gmail.com>
2018-10-21 23:34:21 +0100

    remove redundant file

--- sway/windows.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-OUT=$(rofi -modi "drun,workspaces:python3 $HOME/dotfiles/sway/windows.py" -show-icons -show drun -location 1 -yoffset 30 -me-select-entry '' -me-accept-entry MousePrimary)
-
-if [ "$OUT" != "" ]; then
-    swaymsg workspace $(echo $OUT | awk -F' ' '{print $1}')
-fi
