d35a52a9433839bbc818b7991267ef681a10a820
Abiola Ibrahim <abiola89@gmail.com>
2018-10-24 19:35:33 +0100

    sway updates

--- sway/deps-arch.sh
+++ sway/deps-arch.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sudo pacman -Sy --noconfirm mate-media yay light feh networkmanager dconf
+sudo pacman -Sy --noconfirm mate-media yay light feh networkmanager dconf termite
 
 yay -Sy --noconfirm sway-git wlroots-git slurp-git grim-git mako-git
 
