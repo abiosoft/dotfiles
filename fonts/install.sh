031a1cbf55845d6741e99f6dbef85634c5ac4cd6
Abiola Ibrahim <abiola89@gmail.com>
2018-12-18 07:14:50 +0000

    modify fonts

--- fonts/install.sh
+++ fonts/install.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
 echo installing...
-mkdir -p $HOME/.fonts
-cp ./SF/*.otf $HOME/.fonts
-mkdir -p $HOME/.config/fontconfig/
-cp ./fonts.conf $HOME/.config/fontconfig/fonts.conf
+mkdir -p $HOME/.local/share/fonts
+cp ./SF/*.otf $HOME/.local/share/fonts
+# uncomment the following for arch linux
+# mkdir -p $HOME/.config/fontconfig/
+# cp ./fonts.conf $HOME/.config/fontconfig/fonts.conf
 
 echo resetting cache...
 fc-cache -fv
