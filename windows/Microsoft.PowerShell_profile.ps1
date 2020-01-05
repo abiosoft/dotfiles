93f50762ca488b7d7b5a0590441e5df7e2b4a17b
Abiola Ibrahim <abiola89@gmail.com>
2020-01-05 08:36:44 +0100

    update powershell

--- windows/Microsoft.PowerShell_profile.ps1
+++ windows/Microsoft.PowerShell_profile.ps1
@@ -2,3 +2,7 @@ Set-PSReadLineOption -HistoryNoDuplicates -ShowToolTips
 Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
 Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
 Set-PSReadlineOption -HistorySavePath C:\Users\Abiola\.pshistory.txt
+
+function vim {    
+    bash.exe -c "/usr/local/bin/nvim $args"
+}
