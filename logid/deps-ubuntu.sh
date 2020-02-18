c705c8bd7392e183c9635f7ae8c644167f307b6f
Abiola Ibrahim <abiola89@gmail.com>
2020-02-18 20:35:36 +0100

    add logid

---
+++ logid/deps-ubuntu.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+sudo apt install cmake build-essential libev-dev libudev-dev libconfig-dev libhidapi-dev libevdev-dev libconfig++-dev
+
+# repo
+# https://github.com/PixlOne/logiops
+
+# after install, possible requirement
+# sudo ln -s /usr/local/lib/libhidpp.so /usr/lib/libhidpp.so
