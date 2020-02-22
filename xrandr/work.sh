0aff6e4d708285c70ab347fc401e39bdc3cf3cbf
Abiola Ibrahim <abiola89@gmail.com>
2020-02-22 09:59:50 +0100

    workspace updates

--- xrandr/work.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output VIRTUAL1 --off --output eDP1 --primary --mode 1368x768 --pos 256x1080 --rotate normal --output DP1 --off --output HDMI2 --off --output HDMI1 --mode 1920x1080 --pos 1920x0 --rotate normal --output DP2 --mode 1920x1080 --pos 0x0 --rotate normal
