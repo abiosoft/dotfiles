d60dd7df45f18ad9b562985d1f8ed33754322aaa
Abiola Ibrahim <abiola89@gmail.com>
2020-11-29 10:12:27 +0100

    add emacs

---
+++ emacs/doom.d/config.el
@@ -0,0 +1,74 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets.
+;; (setq user-full-name "John Doe"
+;;       user-mail-address "john@doe.com")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
+;; are the three important ones:
+;;
+;; + `doom-font'
+;; + `doom-variable-pitch-font'
+;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;;
+;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
+;; font string. You generally only need these two:
+(setq doom-font (font-spec :family "JetBrains Mono" :size 14 :weight 'semi-light)
+      doom-variable-pitch-font (font-spec :family "Helvetica Neue" :size 15))
+;; disable bold font
+(setq doom-themes-enable-bold nil)
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+(setq doom-theme 'doom-tomorrow-night)
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/org/")
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type 'relative)
+
+
+;; Here are some additional functions/macros that could help you configure Doom:
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package!' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, including demos of how they are used.
+;;
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; they are implemented.
+
+;; key bindings for macos
+(setq mac-command-modifier      'control
+      ns-command-modifier       'control
+      mac-option-modifier       'meta
+      ns-option-modifier        'meta
+      mac-right-option-modifier 'none
+      ns-right-option-modifier  'none)
+
+;; fix golang lsp
+;; FIXME Fix for emacs 27
+;; https://github.com/emacs-lsp/lsp-mode/issues/1778
+(setq lsp-gopls-codelens nil)
+
+;; keybinding for code navigation
+(map! :n "C-p" #'+ivy/projectile-find-file
+      :n "C-g" #'lsp-ivy-workspace-symbol
+      :nv ",cc" #'comment-line)
---
+++ emacs/install.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+cp -r ./doom.d ~/.doom.d
