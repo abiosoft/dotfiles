5595260db5d224f00ecbcb9972c798ba1a060112
Abiola Ibrahim <abiola89@gmail.com>
2021-01-02 12:10:46 +0100

    emacs: improve org-mode font size

--- emacs/vanilla/libs/org-beautify-theme.el
+++ emacs/vanilla/libs/org-beautify-theme.el
@@ -84,15 +84,15 @@
        (padding nil); `(:line-width 5 :color ,background-color))
        (org-highlights `(:foreground ,base-font-color :background ,secondary-color)))
   (custom-theme-set-faces 'org-beautify
-                          `(org-agenda-structure ((t (:inherit default ,@sans-font :height 1.35 :underline t))))
+                          `(org-agenda-structure ((t (:inherit default ,@sans-font :height 1.40 :underline t))))
                           `(org-level-8 ((t ,headline)))
                           `(org-level-7 ((t ,headline)))
                           `(org-level-6 ((t ,headline)))
                           `(org-level-5 ((t ,headline)))
                           `(org-level-4 ((t ,headline)))
-                          `(org-level-3 ((t (,@headline ,@sans-font :height 1.15 :box ,padding))))
+                          `(org-level-3 ((t (,@headline ,@sans-font :height 1.10 :box ,padding))))
                           `(org-level-2 ((t (,@headline ,@sans-font :height 1.25 :box ,padding))))
-                          `(org-level-1 ((t (,@headline ,@sans-font :height 1.35  :box ,padding ))))
+                          `(org-level-1 ((t (,@headline ,@sans-font :height 1.40  :box ,padding ))))
                           `(org-document-title ((t (:inherit org-level-1 :height 1.5 :underline nil :box ,padding))))
 
                           `(org-block ((t (:foreground ,base-font-color :background ,background-color :box nil))))
