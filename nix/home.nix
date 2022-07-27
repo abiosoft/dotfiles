40dd2255bafbe7e6dcb99bcc95edea9b5afd00aa
Abiola Ibrahim <git@abiosoft.com>
2022-07-27 09:13:24 +0100

    nix: separate packages from home-manager

--- nix/home.nix
+++ nix/home.nix
@@ -5,94 +5,6 @@ let
   homeDir = builtins.getEnv "HOME";
   isMacOS = builtins.currentSystem == "x86_64-darwin" || builtins.currentSystem == "aarch64-darwin";
   link = config.lib.file.mkOutOfStoreSymlink;
-  nixpkgs-unstable = import <nixpkgs-unstable> {
-    # Include the nixos config when importing nixos-unstable
-    # But remove packageOverrides to avoid infinite recursion
-    config = removeAttrs config.nixpkgs.config [ "packageOverrides" ];
-  };
-
-  # cross-platform packages
-  packages = with pkgs; [
-    # things fail to build without these
-    automake
-    autoconf
-    autoconf-archive
-    pkg-config
-    gnumake
-    cmake
-    libtool
-    clang
-    ctags
-
-    # utils
-    bat
-    zsh
-    jq
-    yq
-    tree
-    htop
-    fzf
-    watch
-    ripgrep
-    silver-searcher
-    shellcheck
-    git
-    gnupg
-    # nettools
-    gh
-    nixpkgs-fmt
-    ffmpeg
-    # mkdocs breaking for whatever reason
-
-    # internet
-    youtube-dl
-    wget
-    axel
-    curl
-    speedtest-cli
-
-    # programming/sdks
-    python310
-    ruby
-    go_1_18
-    nodejs-18_x
-    yarn
-    jdk
-    rustup
-    dotnet-sdk
-    postgresql
-
-    # container/devops
-    docker-client
-    docker-compose
-    docker-buildx
-    docker-credential-gcr
-    docker-credential-helpers
-    buildkit
-    kubectl
-    kubectx
-    kubernetes-helm
-    kind
-    terraform
-    vault
-    buildpack
-    google-cloud-sdk
-
-    # virtualization
-    qemu
-  ];
-
-  # macOS specific packages
-  macosPackages = with pkgs; [
-    mas
-  ];
-
-  # Linux specific packages
-  linuxPackages = with pkgs; [
-    coreutils
-  ];
-
-  customPackages = [ ];
 
   # zsh theme
   zshThemeFile = builtins.readFile "${homeDir}/dotfiles/zsh/abiola.zsh-theme";
@@ -149,20 +61,6 @@ in
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
-  # allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
-  # Override select packages to use the unstable channel
-  nixpkgs.config.packageOverrides = pkgs: {
-    go_1_18 = nixpkgs-unstable.go_1_18;
-  };
-
-  # packages
-  home.packages =
-    if isMacOS
-    then packages ++ customPackages ++ macosPackages
-    else packages ++ customPackages ++ linuxPackages;
-
   # utils
   programs.command-not-found.enable = true;
 
@@ -190,6 +88,8 @@ in
       vim-nix
     ];
   };
+  # IntelliJ IdeaVim
+  home.file.".ideavimrc".source = link "${homeDir}/dotfiles/ideavim/ideavimrc";
 
   # tmux
   programs.tmux = {
@@ -212,7 +112,7 @@ in
       export PATH="$PATH:${extraPaths}"
 
       # dotnet tools
-      export DOTNET_ROOT=${dotnetRoot}
+      export DOTNET_ROOT="$(dirname $(dirname $(readlink $(which dotnet))))"
 
       # git commit editor
       export VISUAL=vim
---
+++ nix/packages.nix
@@ -0,0 +1,71 @@
+# uncomment as needed
+
+with import <nixpkgs> { }; [
+  # nix
+  nixpkgs-fmt
+  home-manager
+
+  # things fail to build without these
+  # automake
+  # autoconf
+  # autoconf-archive
+  # pkg-config
+  # gnumake
+  # cmake
+  # libtool
+  # clang
+  # ctags
+
+  # utils
+  # bat
+  # tmux
+  # zsh
+  jq
+  tree
+  htop
+  # fzf
+  # watch
+  # ripgrep
+  # silver-searcher
+  # shellcheck
+  # git
+  # gnupg
+  # nettools
+  # gh
+  # ffmpeg
+  # mkdocs breaking for whatever reason
+
+  # internet
+  # youtube-dl
+  # wget
+  # axel
+  # curl
+  # speedtest-cli
+
+  # programming/sdks
+  python310
+  ruby
+  go_1_18
+  nodejs-18_x
+  yarn
+  # jdk
+  # rustup
+  dotnet-sdk
+  postgresql
+
+  # container/devops
+  docker-client
+  docker-compose
+  docker-buildx
+  docker-credential-gcr
+  docker-credential-helpers
+  # buildkit
+  kubectl
+  # kubectx
+  kubernetes-helm
+  # kind
+  # terraform
+  # vault
+  # buildpack
+  google-cloud-sdk
+]
