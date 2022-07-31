c545860661160efb2c83056ae9f5cc2c2d03c614
Abiola Ibrahim <git@abiosoft.com>
2022-07-31 12:37:36 +0100

    nix: accomodate local package

--- nix/.packages.nix
+++ nix/.packages.nix
@@ -1,78 +1,84 @@
 let
+  homeDir = builtins.getEnv "HOME";
   pkgs = import <nixpkgs> { };
 in
-with pkgs ; [
-  ### uncomment as needed
+## uncomment after cloning source as I am actively working on the source.
+  # let colima = import "${homeDir}/projects/golang/colima"; in [ colima ] ++
+(
+  with pkgs;
+  [
+    ### uncomment as needed
 
-  ## nix
-  nixpkgs-fmt
+    ## nix
+    nixpkgs-fmt
 
-  ## things fail to build without these
-  # automake
-  # autoconf
-  # autoconf-archive
-  # pkg-config
-  # gnumake
-  # cmake
-  # libtool
-  # clang
-  # ctags
+    ## things fail to build without these
+    # automake
+    # autoconf
+    # autoconf-archive
+    # pkg-config
+    # gnumake
+    # cmake
+    # libtool
+    # clang
+    # ctags
 
-  ## shell
-  stow
-  bat
-  tmux
-  zsh
-  neovim
-  oh-my-zsh
-  tree
+    ## shell
+    stow
+    bat
+    tmux
+    zsh
+    neovim
+    oh-my-zsh
+    tree
 
-  ## utils
-  jq
-  htop
-  fzf
-  watch
-  ripgrep
-  silver-searcher
-  shellcheck
-  git
-  delta
-  gh
-  gnupg
-  mkdocs
+    ## utils
+    jq
+    htop
+    fzf
+    watch
+    ripgrep
+    silver-searcher
+    shellcheck
+    git
+    delta
+    gh
+    gnupg
+    mkdocs
 
-  ## internet
-  # youtube-dl
-  # wget
-  # axel
-  # curl
-  # speedtest-cli
+    ## internet
+    # youtube-dl
+    # wget
+    # axel
+    # curl
+    # speedtest-cli
 
-  ## programming/sdks
-  python310
-  ruby
-  go_1_18
-  nodejs-18_x
-  yarn
-  jdk
-  rustup
-  dotnet-sdk
-  postgresql
+    ## programming/sdks
+    python310
+    ruby
+    go_1_18
+    nodejs-18_x
+    yarn
+    jdk
+    rustup
+    dotnet-sdk
+    postgresql
 
-  ## container/devops
-  docker-client
-  docker-compose
-  docker-buildx
-  docker-credential-gcr
-  docker-credential-helpers
-  buildkit
-  kubectl
-  # kubectx
-  kubernetes-helm
-  kind
-  terraform
-  vault
-  buildpack
-  google-cloud-sdk
-]
+    ## container/devops
+    docker-client
+    docker-compose
+    docker-buildx
+    docker-credential-gcr
+    docker-credential-helpers
+    buildkit
+    kubectl
+    # kubectx
+    kubernetes-helm
+    kind
+    terraform
+    vault
+    buildpack
+    google-cloud-sdk
+  ]
+)
 
