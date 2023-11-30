5688085aafe101090ccba838d521323cf45e32e8
Abiola Ibrahim <git@abiosoft.com>
2023-11-30 15:59:36 +0000

    emacs: move from straight.el to elpaca

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -251,23 +251,51 @@ Alternatively, use `doom/window-enlargen'."
 
 
 ;;; Package configs
-;; boostrap straight.el
-(defvar bootstrap-version)
-(let ((bootstrap-file
-       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
-      (bootstrap-version 6))
-  (unless (file-exists-p bootstrap-file)
-    (with-current-buffer
-        (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
-         'silent 'inhibit-cookies)
-
-      (goto-char (point-max))
-      (eval-print-last-sexp)))
-  (load bootstrap-file nil 'nomessage))
-;; setup use-package
-(straight-use-package 'use-package)
-(setq straight-use-package-by-default t)
+;; Elpaca
+(defvar elpaca-installer-version 0.6)
+(defvar elpaca-directory (expand-file-name "elpaca/" user-emacs-directory))
+(defvar elpaca-builds-directory (expand-file-name "builds/" elpaca-directory))
+(defvar elpaca-repos-directory (expand-file-name "repos/" elpaca-directory))
+(defvar elpaca-order '(elpaca :repo "https://github.com/progfolio/elpaca.git"
+                              :ref nil
+                              :files (:defaults "elpaca-test.el" (:exclude "extensions"))
+                              :build (:not elpaca--activate-package)))
+(let* ((repo  (expand-file-name "elpaca/" elpaca-repos-directory))
+       (build (expand-file-name "elpaca/" elpaca-builds-directory))
+       (order (cdr elpaca-order))
+       (default-directory repo))
+  (add-to-list 'load-path (if (file-exists-p build) build repo))
+  (unless (file-exists-p repo)
+    (make-directory repo t)
+    (when (< emacs-major-version 28) (require 'subr-x))
+    (condition-case-unless-debug err
+        (if-let ((buffer (pop-to-buffer-same-window "*elpaca-bootstrap*"))
+                 ((zerop (call-process "git" nil buffer t "clone"
+                                       (plist-get order :repo) repo)))
+                 ((zerop (call-process "git" nil buffer t "checkout"
+                                       (or (plist-get order :ref) "--"))))
+                 (emacs (concat invocation-directory invocation-name))
+                 ((zerop (call-process emacs nil buffer nil "-Q" "-L" "." "--batch"
+                                       "--eval" "(byte-recompile-directory \".\" 0 'force)")))
+                 ((require 'elpaca))
+                 ((elpaca-generate-autoloads "elpaca" repo)))
+            (progn (message "%s" (buffer-string)) (kill-buffer buffer))
+          (error "%s" (with-current-buffer buffer (buffer-string))))
+      ((error) (warn "%s" err) (delete-directory repo 'recursive))))
+  (unless (require 'elpaca-autoloads nil t)
+    (require 'elpaca)
+    (elpaca-generate-autoloads "elpaca" repo)
+    (load "./elpaca-autoloads")))
+(add-hook 'after-init-hook #'elpaca-process-queues)
+(elpaca `(,@elpaca-order))
+;; Install use-package support
+(elpaca elpaca-use-package
+  ;; Enable :elpaca use-package keyword.
+  (elpaca-use-package-mode)
+  ;; Assume :elpaca t unless otherwise specified.
+  (setq elpaca-use-package-by-default t))
+;; Block until current queue processed.
+(elpaca-wait)
 
 
 ;;; Which Key
@@ -370,8 +398,6 @@ Alternatively, use `doom/window-enlargen'."
 (use-package counsel-projectile
   :config
   (counsel-projectile-mode 1))
-(define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
-(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
 
 
 ;;; General life improvements to emacs
@@ -481,11 +507,6 @@ Alternatively, use `doom/window-enlargen'."
 (setq dired-dwim-target t)
 
 
-;;; force all popups to show as a real popup
-(use-package popwin)
-(popwin-mode 1)
-
-
 ;;; Undo
 (use-package undo-tree
   :config
@@ -572,7 +593,7 @@ Alternatively, use `doom/window-enlargen'."
 (use-package ob-typescript)
 ;; go
 (use-package ob-go
-  :straight (ob-go :type git :host github :repo "pope/ob-go"))
+  :elpaca (ob-go :type git :host github :repo "pope/ob-go"))
 ;; http
 (use-package ob-http)
 ;; restclient
@@ -589,6 +610,7 @@ Alternatively, use `doom/window-enlargen'."
                                ("CANCELLED" :strike-through nil :weight normal)))
 
 ;;; org code executions
+(elpaca-wait)
 (setq org-babel-python-command "python3")
 (org-babel-do-load-languages 'org-babel-load-languages
                              '((shell . t)
