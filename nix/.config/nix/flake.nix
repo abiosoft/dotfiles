1be970f6846c2f9f9471a9f7ff3fd0183a64221e
Abiola Ibrahim <git@abiosoft.com>
2024-07-04 06:47:34 +0100

    nix: update to 24.05
    Signed-off-by: Abiola Ibrahim <git@abiosoft.com>

--- nix/.config/nix/flake.nix
+++ nix/.config/nix/flake.nix
@@ -2,7 +2,7 @@
   description = "My dotfiles";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
 
