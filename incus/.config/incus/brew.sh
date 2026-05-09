6201d7629f1e116ffa4affa82a09869f737ac3f1
Abiola Ibrahim <git@abiosoft.com>
2026-05-09 16:26:28 +0100

    incus: update profiles
    Signed-off-by: Abiola Ibrahim <git@abiosoft.com>

---
+++ incus/.config/incus/brew.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -ex
+
+# install homebrew
+NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# add Homebrew to the login profile
+(echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> ~/.profile
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
+# clone dotfiles
+git clone https://github.com/abiosoft/dotfiles ~/.dotfiles --depth 1
+
+# stow dotfiles
+(cd ~/.dotfiles && stow bat brew bin git ideavim neovim tmux zsh)
+
+# install brew packages
+brew bundle -v --file ~/.config/brew/Brewfile
